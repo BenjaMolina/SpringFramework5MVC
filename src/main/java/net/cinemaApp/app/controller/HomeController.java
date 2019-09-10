29e8a9b2f8b11dddd62c17bd75f0d4b236975047
BenjaMolina <molinasb.94@gmail.com>
2019-09-10 18:49:37 -0500

    Creando una Clase (POJO), mandarlo y mostralo en la vista con JSTL

--- src/main/java/net/cinemaApp/app/controller/HomeController.java
+++ src/main/java/net/cinemaApp/app/controller/HomeController.java
@@ -1,5 +1,6 @@
 package net.cinemaApp.app.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,27 +9,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import net.cinemaApp.app.model.Pelicula;
+
 @Controller
 public class HomeController {
+	
+	@RequestMapping(value = "/")
+	public String index(Model model) {
+		
+		List<Pelicula> peliculas = getLista();
+		
+//		peliculas.add("Rapidos y furiosos 5");
+//		peliculas.add("Avengers - End Game");
+//		peliculas.add("El aro 2");
+//		peliculas.add("Aliens vs Depredador");
+		
+		
+		model.addAttribute("peliculas", peliculas);
+		
+		return "home";
+	}
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home() {
 		return "home";
 	}
 	
-	@RequestMapping(value = "/")
-	public String index(Model model) {
-		List<String> peliculas = new LinkedList<>();
-		
-		peliculas.add("Rapidos y furiosos 5");
-		peliculas.add("Avengers - End Game");
-		peliculas.add("El aro 2");
-		peliculas.add("Aliens vs Depredador");
-		
-		model.addAttribute("peliculas", peliculas);
-		
-		return "home";
-	}
 	
 	@RequestMapping(value = "/detail")
 	public String mostrarDetalle(Model model) {
@@ -42,6 +48,48 @@ public class HomeController {
 		model.addAttribute("precio", precioEntrada);
 		
 		return "detalle";
+	}
+	
+	private List<Pelicula> getLista(){
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		List<Pelicula> lista = null;
+		
+		try {
+			lista = new LinkedList<>();
+			
+			Pelicula pelicula1 = new Pelicula();
+			pelicula1.setId(1);
+			pelicula1.setTitulo("Avengers - End Game");
+			pelicula1.setDuracion(120);
+			pelicula1.setClasificacion("B");
+			pelicula1.setGenero("Accion");
+			pelicula1.setFechaEstreno(formatter.parse("05-05-2019"));
+			
+			Pelicula pelicula2 = new Pelicula();
+			pelicula2.setId(2);
+			pelicula2.setTitulo("El Ilusionista");
+			pelicula2.setDuracion(120);
+			pelicula2.setClasificacion("B");
+			pelicula2.setGenero("Accion");
+			pelicula2.setFechaEstreno(formatter.parse("05-05-2009"));
+
+			Pelicula pelicula3 = new Pelicula();
+			pelicula3.setId(3);
+			pelicula3.setTitulo("Juego de Tronos");
+			pelicula3.setDuracion(120);
+			pelicula3.setClasificacion("B");
+			pelicula3.setGenero("Accion");
+			pelicula3.setFechaEstreno(formatter.parse("05-05-2006"));
+			
+			lista.add(pelicula1);
+			lista.add(pelicula2);
+			lista.add(pelicula3);
+			
+		} catch (Exception e) {
+			System.out.println("Error " + e.getMessage());
+		}
+		
+		return lista;
 	}
 
 }
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -22,5 +22,27 @@
 			
 		</c:forEach>
 	</ul>
+	
+	<h2>Tabla de peliculas</h2>
+	<table border="1">
+		<thead>
+			<th>ID</th>
+			<th>Titulo</th>
+			<th>Duracion</th>
+			<th>Clasificacion</th>
+			<th>Genero</th>
+		</thead>
+		<tbody>
+			<c:forEach items="${peliculas}" var="pelicula">
+				<tr>
+					<td> ${pelicula.id} </td>
+					<td> ${pelicula.titulo} </td>
+					<td> ${pelicula.duracion} min</td>
+					<td> ${pelicula.clasificacion} </td>
+					<td> ${pelicula.genero} </td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
 </body>
 </html>
