e32deae40a80f0fdc1e528b218187eac48545099
BenjaMolina <molinasb.94@gmail.com>
2019-09-10 18:02:13 -0500

    Mostrando datos con JSTL (forEach)

--- src/main/java/net/cinemaApp/app/controller/HomeController.java
+++ src/main/java/net/cinemaApp/app/controller/HomeController.java
@@ -1,5 +1,8 @@
 package net.cinemaApp.app.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,23 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/")
+	public String index(Model model) {
+		List<String> peliculas = new LinkedList<>();
+		
+		peliculas.add("Rapidos y furiosos 5");
+		peliculas.add("Avengers - End Game");
+		peliculas.add("El aro 2");
+		peliculas.add("Aliens vs Depredador");
+		
+		model.addAttribute("peliculas", peliculas);
+		
+		return "home";
+	}
+	
 	@RequestMapping(value = "/detail")
 	public String mostrarDetalle(Model model) {
+		
 		String tituloPelicula = "Avengers - End Game";
 		int duracion = 136;
 		double precioEntrada = 50;
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -1,12 +1,26 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+
+<!-- JSTL -->
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Pagina Principal</title>
+<title>Bienvenido a CineMovies</title>
 </head>
 <body>
-	<h1>Bienvenido a la pagina Principal</h1>
+	<h1>Lista de Peliculas</h1>
+	
+	<ul>
+		<c:forEach
+			items="${peliculas}"
+			var="pelicula"
+		>
+			<li> ${pelicula} </li>
+			
+		</c:forEach>
+	</ul>
 </body>
 </html>
