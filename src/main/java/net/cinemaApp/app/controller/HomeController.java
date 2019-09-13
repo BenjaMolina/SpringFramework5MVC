b1873f264aefffc6b6b2df67e4be4beb62a0c131
BenjaMolina <molinasb.94@gmail.com>
2019-09-12 20:24:37 -0500

    Creando ruta dinamica con @PathVariable

--- src/main/java/net/cinemaApp/app/controller/HomeController.java
+++ src/main/java/net/cinemaApp/app/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,15 +24,11 @@ public class HomeController {
 		
 		return "home";
 	}
-	
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String home() {
-		return "home";
-	}
-	
-	
-	@RequestMapping(value = "/detail")
-	public String mostrarDetalle(Model model) {
+
+	@RequestMapping(value = "/detail/{id}", method = RequestMethod.GET)
+	public String mostrarDetalle(Model model, @PathVariable("id") int idPelicula) {
+		
+		System.out.println("Id Pelicula: " + idPelicula);
 		
 		String tituloPelicula = "Avengers - End Game";
 		int duracion = 136;
