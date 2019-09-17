d0bea6a539c2a7de94f2320a5b33894d6887f50d
BenjaMolina <molinasb.94@gmail.com>
2019-09-17 00:08:32 -0500

    RequestParam con metodo GET

--- src/main/java/net/cinemaApp/app/controller/HomeController.java
+++ src/main/java/net/cinemaApp/app/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.cinemaApp.app.model.Pelicula;
 
@@ -29,8 +30,11 @@ public class HomeController {
 		return "home";
 	}
 
-	@RequestMapping(value = "/detail/{id}/{fecha}", method = RequestMethod.GET)
-	public String mostrarDetalle(Model model, @PathVariable("id") int idPelicula, @PathVariable("fecha") String fecha) {
+//	@RequestMapping(value = "/detail/{id}/{fecha}", method = RequestMethod.GET)
+	@RequestMapping(value = "/detail", method = RequestMethod.GET)
+	public String mostrarDetalle(Model model, 
+			@RequestParam(name = "idMovie", required = false) int idPelicula, 
+			@RequestParam("fecha") String fecha) {
 		
 		System.out.println("Id Pelicula: " + idPelicula);
 		System.out.println("Fecha de Busqueda: " + fecha);
