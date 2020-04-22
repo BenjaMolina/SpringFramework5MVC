f1f3485d10aa72e78dce0da8e02ecce8a5bb075f
KAIDO <pain.gedomazo.tensei@gmail.com>
2020-04-22 12:52:20 -0500

    Subir imagenes

--- src/main/java/net/cinemaApp/app/controller/PeliculasController.java
+++ src/main/java/net/cinemaApp/app/controller/PeliculasController.java
@@ -1,8 +1,12 @@
 package net.cinemaApp.app.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import net.cinemaApp.app.model.Pelicula;
@@ -42,7 +48,8 @@ public class PeliculasController {
 	}
 	
 	@PostMapping(value="/save")
-	public String guardar(Pelicula pelicula, BindingResult result, Model model, RedirectAttributes attributes) {
+	public String guardar(Pelicula pelicula, BindingResult result, Model model, RedirectAttributes attributes,
+				@RequestParam("archivoImagen") MultipartFile multipart, HttpServletRequest request) {
 		
 		if(result.hasErrors()) {
 			System.out.println("Existieron errores");
@@ -50,6 +57,11 @@ public class PeliculasController {
 				System.out.println(error.getDefaultMessage());
 			}
 			return "peliculas/formPelicula";
+		}
+		
+		if(!multipart.isEmpty()) {
+			String nombreImagen = guardarImagen(multipart,request);
+			pelicula.setImagen(nombreImagen);
 		}
 		
 		System.out.println(pelicula);
@@ -64,5 +76,29 @@ public class PeliculasController {
 	public void initBinder(WebDataBinder binder) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
+	
+	private String guardarImagen(MultipartFile multipart, HttpServletRequest request) {
+		//Obtenemos el nombre original del archivo
+		String nombreOriginal = multipart.getOriginalFilename();
+		
+		//Obtenemos la ruta absoluta del directorio images
+		//apache-tomcat/webapps/cineapp/resources/images/
+		String rutaFinal = request.getServletContext().getRealPath("/resources/images/");
+		
+		try {
+			//Formamos el nombre del archivo para guardarlo
+			File imageFile = new File(rutaFinal + nombreOriginal);
+			
+			//Guardamos fisicamente el archivo en disco duro
+			multipart.transferTo(imageFile);
+			
+			return nombreOriginal;
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+			return null;
+		}
+
 	}
 }
