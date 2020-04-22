c60407425065bb848bf8a62be89e4a43be569eb2
KAIDO <pain.gedomazo.tensei@gmail.com>
2020-04-21 22:57:39 -0500

    Personalizando DataBinding de Fecha

--- src/main/java/net/cinemaApp/app/controller/PeliculasController.java
+++ src/main/java/net/cinemaApp/app/controller/PeliculasController.java
@@ -1,9 +1,15 @@
 package net.cinemaApp.app.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,5 +37,11 @@ public class PeliculasController {
 		
 		System.out.println(pelicula);
 		return "peliculas/formPelicula";
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
 	}
 }
